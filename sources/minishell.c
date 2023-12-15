cd69ba7aa0633d4080dbd9cde8556bf74aae3a19
Pietro Falaschi <falaschip@gmail.com>
2023-12-15 11:30:34 +0100

    fatto l'expander, ancora da sostituire la lista all'interno della parte mia di programma

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:33:28 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/12/14 09:42:35 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/12/14 16:40:32 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ int	main(int ac, char **av, char **envp)
 				get_cmd_matrix(att.split_arr[att.y], &att);
 				do_builtin(att.arr2, (char **)envp, *env_list);
 				// command(&att);
-				ft_print_array(att.arr2);
+				// ft_print_array(att.arr2);
 				free_arr2(att.arr2, &att);
 				if (!att.split_arr[att.y + 1])
 					break;
