239b2c1160a839c87ae7f2788ed57757352dcf9a
marco <marcocoppola01671@gmail.com>
2023-12-21 12:10:32 +0100

    fix: norminettato

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:16:54 by pfalasch          #+#    #+#             */
-/*   Updated: 2023/12/15 14:13:03 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/12/15 15:06:30 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,7 @@
 
 //global variable
 
-extern int	g_value;
-
-
-
+extern int  g_value;
 
 // colors
 
@@ -76,54 +73,54 @@ extern int	g_value;
 
 typedef struct s_attr
 {
-	//------- n di token -------------------
-	int	nb_tokens;
-	char **split_arr;
-	int y;
-	int x;
-	char **arr2;
-	int y2;
-	int x2;
-	// int *flag$;
-	//------ cmds -------------
-	int count;
-	//----- token variables ---------
-	int count_words;
-	int flag_cmd_valid;
-	int flag_quote;
-	//------ errors -------------------
-	int flag_err_quote;
-	int flag_err_type;
-	int flag_to_do;
-	//-----pipe----------
-	int nb_pipes;
-	int **pipesfd;
-	int pipe_index;
-	//----- flag ----------
-	int write_to_pipe;
-	int read_from_pipe;
-	int read_from_file;
-	int redir;
-	int create_file;
-	int only_create;
-	int heredoc;
-	int skip;
-	//---- envp custom matrix ---
-	char **mx_envp;
-	int y_mx_envp;
-	int x_mx_envp;
+    //------- n di token -------------------
+    int     nb_tokens;
+    char    **split_arr;
+    int     y;
+    int     x;
+    char    **arr2;
+    int     y2;
+    int     x2;
+    // int *flag$;
+    //------ cmds -------------
+    int     count;
+    //----- token variables ---------
+    int     count_words;
+    int     flag_cmd_valid;
+    int     flag_quote;
+    //------ errors -------------------
+    int     flag_err_quote;
+    int     flag_err_type;
+    int     flag_to_do;
+    //-----pipe----------
+    int     nb_pipes;
+    int     **pipesfd;
+    int     pipe_index;
+    //----- flag ----------
+    int     write_to_pipe;
+    int     read_from_pipe;
+    int     read_from_file;
+    int     redir;
+    int     create_file;
+    int     only_create;
+    int     heredoc;
+    int     skip;
+    //---- envp custom matrix ---
+    char    **mx_envp;
+    int     y_mx_envp;
+    int     x_mx_envp;
 
-	//---- expander --------
-	int index;
-	int mem_space;
-	char *check_exp;
-	int len_call_exp;
-	int nb_$;
-	int *flag$;
-	int i_flag$;
-	int *save_y_mx_envp;
+    //---- expander --------
+    int     index;
+    int     mem_space;
+    char    *check_exp;
+    int     len_call_exp;
+    int     nb_$;
+    int     *flag$;
+    int     i_flag$;
+    int     *save_y_mx_envp;
 
-} t_attr;
+}           t_attr;
 
 
 /* ------------------------------ NODE -------------------------------------- */
@@ -134,160 +131,159 @@ typedef struct s_attr
 
 	// init_folder
 
-void	init_parameters(t_attr *att);
-void	init_attributes(t_attr *att);
-void	reinit_parameters(t_attr *att, char **envp);
-void 	start_env(char **envp, t_attr *att);
+void    init_parameters(t_attr *att);
+void    init_attributes(t_attr *att);
+void    reinit_parameters(t_attr *att, char **envp);
+void    start_env(char **envp, t_attr *att);
 
 // tokens_folder
 
 // 00_token_init_count.c
 
-void split_init(char *s, t_attr *att);
-int count_tokens(char *s, t_attr *att);
-int process_special_token(t_attr *att, char *s, int i);
+void    split_init(char *s, t_attr *att);
+int     count_tokens(char *s, t_attr *att);
+int     process_special_token(t_attr *att, char *s, int i);
 
 	// 01_token_in_array.c
 
-void create_array(char *s, t_attr *att);
-char *get_token(char *s);
-void check_next_special_token(char *s, int i, char **token);
+void    create_array(char *s, t_attr *att);
+char    *get_token(char *s);
+void    check_next_special_token(char *s, int i, char **token);
 
 	//02_single_token_matrix.c
-void get_cmd_matrix(char *s, t_attr *att);
-void create_matrix_cmd(char *s, t_attr *att);
-char *get_cmd_token(char *s, t_attr *att);
-void check_cmd_tokens(char *s, t_attr *att);
-int ft_strlen_custom(char *s, int flag, t_attr *att);
-char *ft_write_word(char *s, t_attr *att, int flag, int i);
+void    get_cmd_matrix(char *s, t_attr *att);
+void    create_matrix_cmd(char *s, t_attr *att);
+char    *get_cmd_token(char *s, t_attr *att);
+void    check_cmd_tokens(char *s, t_attr *att);
+int     ft_strlen_custom(char *s, int flag, t_attr *att);
+char    *ft_write_word(char *s, t_attr *att, int flag, int i);
 
 // 03_count_words.c
-int check_single_quotes(char *s, int len, int i, t_attr *att);
-int check_double_quotes(char *s, int len, int i, t_attr *att);
-int check_no_space(char *s, int len, int i, t_attr *att);
-void ft_count_words(char *s, t_attr *att);
+int     check_single_quotes(char *s, int len, int i, t_attr *att);
+int     check_double_quotes(char *s, int len, int i, t_attr *att);
+int     check_no_space(char *s, int len, int i, t_attr *att);
+void    ft_count_words(char *s, t_attr *att);
 
 // 04_expander.c
 
-void copy_expanded_str(t_attr *att, int start, int flag);
-void count_expanded_token(t_attr *att, char *s);
+void    copy_expanded_str(t_attr *att, int start, int flag);
+void    count_expanded_token(t_attr *att, char *s);
 
 // utilities_folder
 
 // prompt.c
-char *prompt(void);
+char    *prompt(void);
 
 // signals.c
-void	set_signals(void);
-void 	handle_interrupt(int sig);
+void    set_signals(void);
+void    handle_interrupt(int sig);
 
 // debugg.c
 
-void	ft_print_array(char **array);
+void    ft_print_array(char **array);
 
 // 00_utils.c
 
-int ft_strcmp(const char *str1, const char *str2);
-char *ft_strcpy(char *destination, const char *source);
-char *ft_strcat(char *dest, const char *src);
-char *ft_strncpy(char *dest, const char *src, size_t n);
+int     t_strcmp(const char *str1, const char *str2);
+char    *ft_strcpy(char *destination, const char *source);
+char    *ft_strcat(char *dest, const char *src);
+char    *ft_strncpy(char *dest, const char *src, size_t n);
 
 // 01_utils.c
 
-t_list *copy_env_in_list(char **env);
-t_list *sort_list(t_list *list);
-void insert_between_node(t_list *prev, char *content);
+t_list  *copy_env_in_list(char **env);
+t_list  *sort_list(t_list *list);
+void    insert_between_node(t_list *prev, char *content);
 char    *get_var_name(char *full_var);
 
 //	error_folder
 
 // 00_verify_readline.c
-int verify_readline(char *s, t_attr *att);
+int     verify_readline(char *s, t_attr *att);
 
 // 01_error_quotes.c
 
-int count_quotes(char *s);
+int     count_quotes(char *s);
 
 // 02_error_error_start_last.c
 
-int error_begin(char *s);
-int error_end(char *s, char c);
+int     error_begin(char *s);
+int     error_end(char *s, char c);
 
 // 03_error_redir.c
 
-int check_extra_char(char *s, int i);
-int check_next_arg(char *s, int i);
-int check_2nd_char(char *s, int i);
-int ft_scorri(char *s, int i);
-int error_mixed_start(char *s);
+int     check_extra_char(char *s, int i);
+int     check_next_arg(char *s, int i);
+int     check_2nd_char(char *s, int i);
+int     ft_scorri(char *s, int i);
+int     error_mixed_start(char *s);
 
 // 04_error_mixed.c
 
-int return_not_required(void);
-int return_pipe_error(void);
-int return_gt_error(void);
-int return_lt_error(void);
-int return_nl_error(void);
+int     return_not_required(void);
+int     return_pipe_error(void);
+int     return_gt_error(void);
+int     return_lt_error(void);
+int     return_nl_error(void);
 
 // 05_error_dollar.c
 
-int error_dollar(char *s, t_attr *att);
-int error_dollar_02(char *s, int i, t_attr *att);
-int error_dollar_03(char *check_envp, t_attr *att, int len);
+int     error_dollar(char *s, t_attr *att);
+int     error_dollar_02(char *s, int i, t_attr *att);
+int     error_dollar_03(char *check_envp, t_attr *att, int len);
 
 //	free_folder
 
 // 00_free.c
 
-void free_arr2(char **tokens, t_attr *att);
-void free_arr(char **arr);
-void ft_delete_matrix(void *matrix);
-void free_mx_envp(t_attr *att);
+void    free_arr2(char **tokens, t_attr *att);
+void    free_arr(char **arr);
+void    ft_delete_matrix(void *matrix);
+void    free_mx_envp(t_attr *att);
 
 // Commands
 
-int do_builtin(char **args, char **env, t_list env_list);
-int		pwd(char **env);
-int		envi(char **env);
-int		ls_l(char **env, int j);
-int		echo(char **args);
-void	ft_exit(void);
+int     do_builtin(char **args, char **env, t_list env_list);
+int     pwd(char **env);
+int     envi(char **env);
+int     ls_l(char **env, int j);
+int     echo(char **args);
+void    ft_exit(void);
 
 // 01_export
 
-int ft_export(char **args, t_list env_list);
+int     ft_export(char **args, t_list env_list);
 
 // 01_unset
 
-int ft_unset(char **args, t_list *env_list);
+int     ft_unset(char **args, t_list *env_list);
 void    ft_remove_node(t_list **env_list, char *arg);
 
 // parser folder
 
 // parser.c
 
-void reset_flags(t_attr *att);
-void next_step_sub2(t_attr *att);
-void next_step_sub(t_attr *att);
-int check_next_step(t_attr *att);
+void    reset_flags(t_attr *att);
+void    next_step_sub2(t_attr *att);
+void    next_step_sub(t_attr *att);
+int     check_next_step(t_attr *att);
 
 // lexer.c
 
-void command(t_attr *att);
+void    command(t_attr *att);
 
 // Executer folder
 
 //	pipe.c
 
-int count_pipes(t_attr *att);
-void init_pipes(t_attr *att);
-void write_to_pipe(t_attr *att);
-void read_from_pipe(t_attr *att);
-void close_pipeline(t_attr *att);
+int     count_pipes(t_attr *att);
+void    init_pipes(t_attr *att);
+void    write_to_pipe(t_attr *att);
+void    read_from_pipe(t_attr *att);
+void    close_pipeline(t_attr *att);
 
 // expander
 
 // 00_expander.c
-
 
 #endif
--- sources/Commands/export.c
+++ sources/Commands/export.c
@@ -6,7 +6,7 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 17:50:37 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/12/15 11:36:12 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/12/21 12:07:17 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 #include <stdlib.h>
 #include "../../includes/minishell.h"
 
-int basic_export(t_list *env_list)
+int	basic_export(t_list *env_list)
 {
-    t_list *export;
+	t_list	*export;
 
-    export = sort_list(env_list);
-    while (export)
-    {
-        printf("declare -x %s\n", (char *)export->content);
-        export=export->next;
-    }
-    return (0);
+	export = sort_list(env_list);
+	while (export)
+	{
+		printf("declare -x %s\n", (char *)export->content);
+		export = export->next;
+	}
+	return (0);
 }
 
-int add_on_env(char *arg, t_list env_list)
+int	add_on_env(char *arg, t_list env_list)
 {
     // appena hai la lista di env, devi cercare se la variabile esiste già o meno (con ft_strncmp)
     // e se esiste devi aggiornarla (con ft_strjoin)
@@ -40,42 +40,36 @@ int add_on_env(char *arg, t_list env_list)
     // e come "next" della stringa da aggiungere il "next" del nodo precedente
     // --Fermarsi quando la lista è finita e aggiungere alla fine
     // SE PERO tutti i caratteri sono uguali allora sostituisci tutto il content con la stringa attuale
-    t_list  *env;
+	t_list	*env;
 
-    env = &env_list;
-
-    while (env)
-    {
-        printf("sss %d", ft_strcmp(arg, (char *)env->content));
-        env = env->next;
-    }
-    return (0);
+	env = &env_list;
+	while (env)
+	{
+		printf("sss %d", ft_strcmp(arg, (char *)env->content));
+		env = env->next;
+	}
+	return (0);
 }
 
-int ft_export(char **args, t_list env_list)
+int	ft_export(char **args, t_list env_list)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (args[i])
-    {
-        // printf("Args: %s\n", args[i]);
-        i++;
-    }
-    if (i == 1)
-        return (basic_export(&env_list));
-    else
-    {
-        args++;
-        while (*args)
-        {
-            if(!add_on_env(*args, env_list))
-                args++;
-            else
-                return (1);
-        }
-    }
-
-    return (0);
+	i = 0;
+	while (args[i])
+		i++;
+	if (i == 1)
+		return (basic_export(&env_list));
+	else
+	{
+		args++;
+		while (*args)
+		{
+			if (!add_on_env(*args, env_list))
+				args++;
+			else
+				return (1);
+		}
+	}
+	return (0);
 }
-
