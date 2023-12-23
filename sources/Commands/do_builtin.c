8c34d37ab56c056073bb614f3622d765fe2248f7
marco <marcocoppola01671@gmail.com>
2023-12-23 17:13:38 +0100

    feat: handled command not found

--- sources/Commands/do_builtin.c
+++ sources/Commands/do_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   do_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alesac <alesac@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 14:34:32 by alesac            #+#    #+#             */
-/*   Updated: 2023/12/22 15:53:13 by alesac           ###   ########.fr       */
+/*   Updated: 2023/12/23 16:40:42 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,28 @@
 
 int	do_builtin(char **args, char **env, t_list env_list)
 {
-	int 	i;
+	int	i;
+
 	i = 0;
 	while (args[i])
 	{
-		if (ft_strncmp(args[i], "export\0", 7) == 0 || ft_strncmp(args[i], "unset\0", 6) == 0 || ft_strncmp(args[i], "echo\0", 5) == 0 || ft_strncmp(args[i], "pwd\0", 4) == 0 || ft_strncmp(args[i], "env\0", 4) == 0 || ft_strncmp(args[i], "ls\0", 3) == 0 || ft_strncmp(args[i], "ls -l\0", 6) == 0 || strstr(args[i], "echo\0") != NULL || ft_strncmp(args[i], "exit\0", 5) == 0 || ft_strncmp(args[i], "cd\0", 3) == 0)
-		{
-			if (ft_strncmp(args[i], "pwd\0", 4) == 0)
-				return (pwd((char **) env));
-			if (ft_strncmp(args[i], "export\0", 7) == 0)
-				return (ft_export((char **) args, env_list));
-			if (ft_strncmp(args[i], "unset\0", 6) == 0)
-				return (ft_unset((char **) args, &env_list));
-			if (ft_strncmp(args[i], "env\0", 4) == 0)
-				return (envi((char **) env));
-			if (ft_strncmp(args[i], "ls\0", 3) == 0)
-				return (ls_l((char **) env, 0));
-			if (strstr(args[i], "echo") != NULL)
-				return (echo((char **) args));
-			if (ft_strncmp(args[i], "exit\0", 5) == 0)
-				ft_exit();
-			if (ft_strncmp(args[i], "cd\0", 3) == 0)
-				ft_cd((char **) args);
-		}
+		if (ft_strncmp(args[i], "pwd\0", 4) == 0)
+			return (pwd((char **) env));
+		if (ft_strncmp(args[i], "export\0", 7) == 0)
+			return (ft_export((char **) args, env_list));
+		if (ft_strncmp(args[i], "unset\0", 6) == 0)
+			return (ft_unset((char **) args, &env_list));
+		if (ft_strncmp(args[i], "env\0", 4) == 0)
+			return (envi((char **) env));
+		if (ft_strncmp(args[i], "ls\0", 3) == 0)
+			return (ls_l((char **) env, 0));
+		if (strstr(args[i], "echo") != NULL)
+			return (echo((char **) args));
+		if (ft_strncmp(args[i], "exit\0", 5) == 0)
+			ft_exit();
+		if (ft_strncmp(args[i], "cd\0", 3) == 0)
+			ft_cd((char **) args);
+		printf("%s: command not found\n", args[i]);
 		i++;
 	}
 	return (0);
