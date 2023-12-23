b73170e621b243ce1f7c0b80c046c73041d13294
Alesac1 <alesachi1907@gmail.com>
2023-12-23 03:34:13 +0100

    aggiustatols

--- sources/Commands/builtin.c
+++ sources/Commands/builtin.c
@@ -6,55 +6,11 @@
 /*   By: alesac <alesac@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:55:31 by alesac            #+#    #+#             */
-/*   Updated: 2023/12/22 10:39:47 by alesac           ###   ########.fr       */
+/*   Updated: 2023/12/23 03:32:16 by alesac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-int	do_builtin(char **args, char **env, t_list env_list)
-{
-	int 	i;
-	i = 0;
-	while (args[i])
-	{
-		if (ft_strncmp(args[i], "export\0", 7) == 0 || ft_strncmp(args[i], "unset\0", 6) == 0 || ft_strncmp(args[i], "echo\0", 5) == 0 || ft_strncmp(args[i], "pwd\0", 4) == 0 || ft_strncmp(args[i], "env\0", 4) == 0 || ft_strncmp(args[i], "ls\0", 3) == 0 || ft_strncmp(args[i], "ls -l\0", 6) == 0 || strstr(args[i], "echo\0") != NULL || ft_strncmp(args[i], "exit\0", 5) == 0 || ft_strncmp(args[i], "cd\0", 3) == 0)
-		{
-			// if (ft_strncmp(args[i], "echo\0", 5) == 0)
-			// 	return (echo((char **) args));
-			if (ft_strncmp(args[i], "pwd\0", 4) == 0)
-				return (pwd((char **) env));
-
-			if (ft_strncmp(args[i], "export\0", 7) == 0)
-				return (ft_export((char **) args, env_list));
-
-			if (ft_strncmp(args[i], "unset\0", 6) == 0)
-				return (ft_unset((char **) args, &env_list));
-
-			if (ft_strncmp(args[i], "env\0", 4) == 0)
-				return (envi((char **) env));
-
-			if (ft_strncmp(args[i], "ls -l\0", 6) == 0)
-				return (ls_l((char **) env, 1));
-
-			if (ft_strncmp(args[i], "ls\0", 3) == 0)
-				return (ls_l((char **) env, 0));
-
-			if (strstr(args[i], "echo") != NULL)
-				return (echo((char **) args));
-
-			if (ft_strncmp(args[i], "exit\0", 5) == 0)
-				ft_exit();
-			if (ft_strncmp(args[i], "cd\0", 3) == 0)
-				ft_cd((char **) args);
-		}
-		//else
-			//printf("Comando «%s» non trovato\n", args[i]);
-		i++;
-	}
-	return (0);
-}
-
 
 int	echo(char **args)
 {
@@ -125,6 +81,12 @@ int	ls_l(char **env, int j)
 		perror("execve failed");
 		exit(1);
 	}
+	else
+	{
+		int	status;
+		
+		waitpid(child_pid, &status, 0);
+	}
 	return (0);
 }
 
@@ -136,18 +98,10 @@ void	ft_cd(char **args)
 	{
 		tmp = getenv("HOME");
 		chdir(tmp);
-		//cd_path(main);
 	}
 	else if (chdir(args[1]) == -1)
 	{
 		printf(RED"cd : diocane non ce sta la cartella");
 		printf("\n");
 	}
-	//else
-		//cd_path(main);
-}
-
-void	ft_exit()
-{
-	exit(1);
 }
