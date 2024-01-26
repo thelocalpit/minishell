ce9c908f704654130d85167d0551308cdbdf3c29
Deggio <danideg12@gmail.com>
2024-01-26 05:21:11 +0100

    Read_file < ora riconosce i path, però il codice fa in SEGFAULT da qualche parte

--- sources/executer/read_file.c
+++ sources/executer/read_file.c
@@ -6,7 +6,7 @@
 /*   By: deggio <deggio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 19:23:19 by deggio            #+#    #+#             */
-/*   Updated: 2024/01/11 06:37:53 by deggio           ###   ########.fr       */
+/*   Updated: 2024/01/26 05:15:32 by deggio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ int	red_input(t_attr *att, char *path)
 {
 	att->red_fd = open(path, O_RDONLY);
 	if (att->red_fd < 0)
-	{
-		perror("cannot open file");
-		return (-1);
-	}
+		return (1);
 	dup2(att->red_fd, 0);
 	close(att->red_fd);
 	return (0);
@@ -47,7 +44,9 @@ int	heredoc(t_attr *att)
 	}
 	free(eof);
 	close(fd);
-	red_input(att, "objects/heredoc");
+	if (red_input(att, "objects/heredoc"))
+		printf("minishell: %s: No such file or directory\n",
+			att->split_arr[att->y + 2]);
 	unlink("objects/heredoc");
 	return (0);
 }
@@ -55,9 +54,25 @@ int	heredoc(t_attr *att)
 int	read_from_file(t_attr *att)
 {
 	char	*file_path;
+	char	*current_path;
+	char	*tmp;
 
 	file_path = ft_strtrim(att->split_arr[att->y + 2], " ");
-	red_input(att, file_path);
+	if (file_path[0] != '/')
+	{
+		current_path = malloc(sizeof(char) * PATH_MAX);
+		getcwd(current_path, PATH_MAX);
+		tmp = ft_strjoin(current_path, "/");
+		free(current_path);
+		current_path = ft_strjoin(tmp, file_path);
+		free(tmp);
+		free(file_path);
+		file_path = ft_strdup(current_path);
+		free(current_path);
+	}
+	if (red_input(att, file_path))
+		printf("minishell: %s: No such file or directory\n",
+			att->split_arr[att->y + 2]);
 	free(file_path);
 	return (0);
 }
