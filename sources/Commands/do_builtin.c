66bf41ae20e7b24f792378a66c0e0b4c37408cd9
DaniDeggio <danideg12@gmail.com>
2024-02-03 19:08:37 +0100

    ora skippa le builtin prima dell exec se c è redirecrtion o pipe

--- sources/Commands/do_builtin.c
+++ sources/Commands/do_builtin.c
@@ -6,7 +6,7 @@
 /*   By: deggio <deggio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 14:34:32 by alesac            #+#    #+#             */
-/*   Updated: 2024/01/30 17:24:08 by deggio           ###   ########.fr       */
+/*   Updated: 2024/02/03 19:05:31 by deggio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,28 @@
 
 int	do_builtin(t_attr *att)
 {
-	if (ft_strncmp(att->arr2[0], "export\0", 7) == 0)
-		return (ft_export(att->arr2, att));
-	else if (ft_strncmp(att->arr2[0], "unset\0", 6) == 0)
-		return (ft_unset(att->arr2, att));
-	else if (ft_strncmp(att->arr2[0], "exit\0", 5) == 0)
+	if (!att->write_to_pipe && ! att->read_from_pipe
+		&& !att->read_from_file && !att->redir && !att->heredoc
+		&& !att->skip)
 	{
-		ft_exit_custom(att);
-		// ft_exit();
-		return (0);
+		if (ft_strncmp(att->arr2[0], "export\0", 7) == 0)
+			return (ft_export(att->arr2, att));
+		else if (ft_strncmp(att->arr2[0], "unset\0", 6) == 0)
+			return (ft_unset(att->arr2, att));
+		else if (ft_strncmp(att->arr2[0], "exit\0", 5) == 0)
+		{
+			ft_exit_custom(att);
+			// ft_exit();
+			return (0);
+		}
+		else if (ft_strncmp(att->arr2[0], "cd\0", 3) == 0)
+			return (ft_cd(att->arr2));
+		else if (ft_strchr(att->arr2[0], '=') != NULL
+			&& ((att->arr2[0][0] >= 65 && att->arr2[0][0] <= 90)
+			|| (att->arr2[0][0] >= 97 && att->arr2[0][0] <= 122)))
+			return (add_var(att->arr2[0], att));
 	}
-	else if (ft_strncmp(att->arr2[0], "cd\0", 3) == 0)
-		return (ft_cd(att->arr2));
-	else if (ft_strchr(att->arr2[0], '=') != NULL
-		&& ((att->arr2[0][0] >= 65 && att->arr2[0][0] <= 90)
-		|| (att->arr2[0][0] >= 97 && att->arr2[0][0] <= 122)))
-		return (add_var(att->arr2[0], att));
-	else
-		exec(att);
+	exec(att);
 	set_signals();
 	return (0);
 }
